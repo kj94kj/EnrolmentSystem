481625ca139110b9ea6e1711fc1b93a2846045a3
kj94kj <103031915+kj94kj@users.noreply.github.com>
2024-05-31 16:53:39 +0900

    DB연결 안한 로그인 기능

---
+++ login/LoginProcess.java
@@ -0,0 +1,9 @@
+package login;
+
+public class LoginProcess {
+	public boolean isLogin(String loginid, String password) {
+		//DB에서 로그인 정보를 찾으면 UserInfo에 userid, userType를 담는다.
+		//! DB연결 추가구현해야함.
+		return true;
+	}
+}
