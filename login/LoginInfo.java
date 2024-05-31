d6ce09ff75dac609540e6967dc6d6bd0365e70e2
kj94kj <103031915+kj94kj@users.noreply.github.com>
2024-05-31 18:42:03 +0900

    .

--- login/LoginInfo.java
+++
@@ -1,8 +0,0 @@
-package login;
-
-public class LoginInfo {
-	//userid, userType를 담는다.
-	//!! userType에따라 자식객체를 만들어서(StudentInfo, ProfessorInfo, AdminInfo) 저장하는거 구현.
-	protected int userid;
-	protected int userType;	
-}
--- login/LoginProcess.java
+++
@@ -1,9 +0,0 @@
-package login;
-
-public class LoginProcess {
-	public boolean isLogin(String loginid, String password) {
-		//DB에서 로그인 정보를 찾으면 UserInfo에 userid, userType를 담는다.
-		//! DB연결 추가구현해야함.
-		return true;
-	}
-}
